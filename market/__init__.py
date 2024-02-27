3f17df15697ec4a7797bc8588d16589993d4945e
jjcoll <jjordicoll@gmail.com>
2024-02-27 10:20:17 +0100

    first commit

---
+++ market/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+app = Flask(__name__)
+
+# Construct the path to the database file inside the 'market' package
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+db = SQLAlchemy(app)
+
+from market import routes
+
+
+# with app.app_context():
+
+#     #     db.session.query(Item).delete()
+#     #     db.create_all()
+#     #     db.session.commit()
+#     #     items = Item.query.all()
+#     #     print(items)
+#     for item in Item.query.all():
+#         print(item.name)
+
+#     # Filter for items that cost 500 dollars
+#     for item in Item.query.filter_by(price=500):
+#         print(item.name)
---
+++ run.py
@@ -0,0 +1,6 @@
+from market import app
+
+# imports app from market package and it will run it
+# checks if run.py file is executed directly
+if __name__ == "__main__":
+    app.run(debug=True)
